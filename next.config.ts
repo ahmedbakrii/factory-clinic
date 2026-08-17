57f61472c08b99c8af8a1754aa885419f8b71a5b
Ahmed Bakri <ahmedbakrii202@gmail.com>
2026-08-17 12:10:21 +0300

    Fix: Force webpack for production build

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,16 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  disable: process.env.NODE_ENV === "development", // عشان ميهنجش وإنت شغال على اللاب
+  disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // السطر ده هو الحل اللي هيخلي الـ build يعدي من غير اعتراض
+  experimental: {
+    turbopack: undefined, 
+  },
 };
 
 export default withPWA(nextConfig);
